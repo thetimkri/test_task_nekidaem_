f6b51da54346c58273d01f4e31549be38be905fd
Your Name <you@example.com>
2024-02-11 00:04:48 +0300

    add views Post and Subscriptions

--- myproject/blog/views.py
+++ myproject/blog/views.py
@@ -1,7 +1,15 @@
 from rest_framework import viewsets
-from .models import Blog
-from .serializers import BlogSerializer
+from .models import Blog,Post,Subscription
+from .serializers import BlogSerializer,PostSerializer,SubscriptionSerializer
 
 class BlogViewSet(viewsets.ModelViewSet):
     queryset = Blog.objects.all()
     serializer_class = BlogSerializer
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+class SubscriptionViewSet(viewsets.ModelViewSet):
+    queryset = Subscription.objects.all()
+    serializer_class = SubscriptionSerializer
