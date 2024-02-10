1cf452b428703376d15a62f59b65688a8fca4ba3
Your Name <you@example.com>
2024-02-11 00:11:26 +0300

    add model ReadPost

--- myproject/blog/models.py
+++ myproject/blog/models.py
@@ -37,3 +37,14 @@ class Subscription(models.Model):
 
     def __str__(self):
         return f'{self.subscriber.username} -> {self.blog.owner.username}'
+
+class ReadPost(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='read_posts')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='read_by')
+    read_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'post')
+
+    def __str__(self):
+        return f"{self.user.username} read {self.post.title}"
