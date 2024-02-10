0fa66a3942f22e587d1f3a2eeb3d0712987bf4b3
Your Name <you@example.com>
2024-02-10 22:53:49 +0300

    add urls and views Blog

--- myproject/blog/views.py
+++ myproject/blog/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Blog
+from .serializers import BlogSerializer
 
-# Create your views here.
+class BlogViewSet(viewsets.ModelViewSet):
+    queryset = Blog.objects.all()
+    serializer_class = BlogSerializer
