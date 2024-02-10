9a19cf1492c7a1a38a8e59e99ad9ceea1ee5f50d
Your Name <you@example.com>
2024-02-11 00:22:58 +0300

    add UserFeedView and urls

--- myproject/blog/views.py
+++ myproject/blog/views.py
@@ -1,6 +1,12 @@
 from rest_framework import viewsets
-from .models import Blog,Post,Subscription
+from .models import Blog,Post,Subscription,ReadPost
 from .serializers import BlogSerializer,PostSerializer,SubscriptionSerializer
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.settings import api_settings
 
 class BlogViewSet(viewsets.ModelViewSet):
     queryset = Blog.objects.all()
@@ -10,6 +16,24 @@ class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
+    @action(detail=True, methods=['post'], permission_classes=[IsAuthenticated])
+    def mark_as_read(self, request, pk=None):
+        post = self.get_object()
+        user = request.user
+        ReadPost.objects.get_or_create(user=user, post=post)
+        return Response({'status': 'post marked as read'}, status=status.HTTP_200_OK)
+
 class SubscriptionViewSet(viewsets.ModelViewSet):
     queryset = Subscription.objects.all()
     serializer_class = SubscriptionSerializer
+
+class UserFeedView(generics.ListAPIView):
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticated]
+    pagination_class = api_settings.DEFAULT_PAGINATION_CLASS
+
+    def get_queryset(self):
+        user = self.request.user
+        subscribed_blogs = Subscription.objects.filter(subscriber=user).values_list('blog', flat=True)
+        queryset = Post.objects.filter(blog__in=subscribed_blogs).order_by('-created_at')[:500]
+        return queryset
--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include,re_path
 from rest_framework.routers import DefaultRouter
 from rest_framework import permissions
 
-from blog.views import BlogViewSet,PostViewSet,SubscriptionViewSet
+from blog.views import BlogViewSet,PostViewSet,SubscriptionViewSet,UserFeedView
 
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
@@ -23,9 +23,11 @@ schema_view = get_schema_view(
    permission_classes=(permissions.AllowAny,),
 )
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('feed/', UserFeedView.as_view(), name='user-feed'),
 ]
