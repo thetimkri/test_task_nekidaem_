4d271200dd1c98cde76486ad285bed9e2e01050d
Your Name <you@example.com>
2024-02-10 23:27:18 +0300

    add Swagger

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -1,13 +1,29 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include,re_path
 from rest_framework.routers import DefaultRouter
+from rest_framework import permissions
 
 from blog.views import BlogViewSet
+
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 router = DefaultRouter()
 router.register(r'blogs',BlogViewSet)
 
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Blog API",
+      default_version='v1',
+      description="API for managing user blogs",
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
